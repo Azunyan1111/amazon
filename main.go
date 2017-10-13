50b4fb95ccbbe59732bae7ec44a1ab29ef78619d
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-10-13 20:31:05 +0900

    add echo http server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "log"
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/Azunyan1111/amazon/handler"
+)
 
 func main() {
-	log.Println("Hello World")
+	// Echoのインスタンス作る
+	e := echo.New()
+
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/", handler.MainPage())
+
+	// サーバー起動
+	e.Start(":8888")    //ポート番号指定してね
 }
