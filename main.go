667485b2ffc798dc41c0aa4600abc9a8f27193bf
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-10-23 19:08:35 +0900

    deploy test4

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 			go func() { model.GetRankingASIN() }() //ok
 			// 1日1回実行すると適当に拾ってきたASINリストから価格情報を取得して格納する。
 			go func() { model.GetPrice() }()
-			time.Sleep(time.Hour * 24)
+			time.Sleep(time.Hour * 1)
 		}
 	}()
 
